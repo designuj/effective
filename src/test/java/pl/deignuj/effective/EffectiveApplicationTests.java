99003a2514b97151c11d2b83819b9ffaddafe888
tom <26408576+designuj@users.noreply.github.com>
2021-05-06 23:51:30 +0100

    init empty Spring Boot project

---
+++ src/test/java/pl/deignuj/effective/EffectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.deignuj.effective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
